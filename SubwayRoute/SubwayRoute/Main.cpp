f33d4f93a5209d5b53e161ece9ca7218919501fb
TheLastSummer <1021369745@qq.com>
2016-09-29 22:10:20 +0800

    API封装
    1. 那个画图的部分我试了不能动，openGL函数的参数不能是实参，那些方法都不能放到其他类里。
    2.站点和id的匹配算了吧，我试了一下，几乎都要改，太麻烦了。

--- SubwayRoute/SubwayRoute/Main.cpp
+++ SubwayRoute/SubwayRoute/Main.cpp
@@ -2,7 +2,7 @@
 #include <tchar.h>
 #include "ReadMap.h"
 #include "calculateRoute.h"
-#include "GUI.h"
+#include "subwayNet.h"
 #include <iostream>
 #include <fstream>
 #include <GL/glut.h>
@@ -13,12 +13,6 @@
 #include <time.h>
 using namespace std;
 
-//获取并输出某条路线上的所有站点
-void getStations(string name);
-//计算并输出所求路径
-bool getRoute(string str, string begin, string end);
-//将参数和求得的路线保存到文件中
-void saveData(string args);
 
 //画图函数
 void drawCNString(const char* str);
@@ -26,25 +20,15 @@ void selectFont(int size, int charset, const char* face);
 void display();
 void animation();
 void OnTimer(int value);
-
-ReadMap rm;
-calculateRoute cr;
-vector<string> route1;     //通过计算得到的路线,不带换乘信息
-vector<string> route2;     //通过计算得到的路线,带换乘信息
-int bt = 0; //-b的次数
-int ct = 0; //-c的次数
-
-
 //画图参数
 const int n = 50;
 const GLfloat R = 0.01f;
 const GLfloat Pi = 3.1415926536f;
 #define MAX_CHAR 128
-int step = 0;
-
+//画图线程
 int ac;
 char **av;
-//画图线程
+int step = 0;
 DWORD WINAPI draw(LPVOID lpParam) {
 	step = 0;
 	//初始化
@@ -66,156 +50,64 @@ DWORD WINAPI draw(LPVOID lpParam) {
 	return 0;
 }
 
-
+subwayNet sn;
 int main(int argc, char *argv[]) {
 
 	DWORD dwThread;
 	ac = argc;
 	av = argv;
-	//读取地图信息
-	cout << "read map ..." << endl;
-	if (!rm.readFile()) {
-		cout << "map is wrong !" << endl;
-	}
-	if (!rm.setInf()) {
-		cout << "map is wrong !" << endl;
-	}
-	if (!rm.readPosi()) {
-		cout << "map is wrong !" << endl;
-	}
-	cout << "finished" << endl;
-	cr.init(rm);
 	
+
 	//清空保存的文件
 	remove("saveData.txt");
-
 	//读取参数
 	string str;
 	string beginSta, endSta;
 	while (true) {
 		cout << "Please input the args：" << endl;
 		cin >> str;
-		if (str == "-b" || str == "-c") {
-			//记录偏好
-			if (str == "-b") {
-				bt++;
-			}
-			else {
-				ct++;
-			}
+		if (str == "-b") {
 			cin >> beginSta;
 			cin >> endSta;
-			getRoute(str,beginSta,endSta);
-			str +=" "+ beginSta +" "+ endSta;
-			saveData(str);
+			sn.getRouteB(beginSta, endSta);
+		}
+		else if (str == "-c") {
+			cin >> beginSta;
+			cin >> endSta;
+			sn.getRouteC(beginSta, endSta);
 		}
 		else if (str == "-g") {
 			cin >> beginSta;
 			cin >> endSta;
 			//通过偏好计算出用哪种方式
-			if (bt >= ct) {
-				getRoute("-b", beginSta, endSta);
-			}
-			else {
-				getRoute("-c", beginSta, endSta);
-			}
-			saveData("-g "+ beginSta+" "+ endSta);
-
+			sn.getRouteG(beginSta, endSta);		
 			CreateThread(NULL, 0, draw, NULL, 0, &dwThread);
-			
 		}
 		else {
-			getStations(str);
-			saveData(str);
+			sn.getStations(str);
 		}
 	}
 	
 	return 0;
 }
 
-bool getRoute(string str, string begin, string end) {
-	int i = 0;
-	vector<vector<string>> route;
-	if (str == "-b") {
-		if (!(rm.ifContain_1(rm.stationQueen, begin) >= 0 && rm.ifContain_1(rm.stationQueen, end) >= 0)) {
-			cout << "error args" << endl;
-			return false;
-		}
-		route = cr.addTransferInf(cr.getMinStaRoute(begin, end));
-		route1 = route[0];
-		route2 = route[1];
-	}
-	else if (str == "-c") {
-		if (!(rm.ifContain_1(rm.stationQueen, begin) >= 0 && rm.ifContain_1(rm.stationQueen, end) >= 0)) {
-			cout << "error args" << endl;
-			return false;
-		}
-		route = cr.addTransferInf(cr.getMinStaOfMinTraRoute(cr.getAllMinTraRoute(begin, end)));
-		route1 = route[0];
-		route2 = route[1];
-	}
-	else {
-		cout << "错误参数" << endl;
-		return false;
-	}
 
-	return true;
 
-}
-
-void getStations(string name) {
-	string str = "该线路不存在与地图中";
-	if (!rm.printRoute(name)) {
-		cout << str << endl;
-	}
-}
-
-void saveData(string args) {
-	ofstream outfile("saveData.txt", ios::app);
-	if (!outfile) {
-		cout << "error in open saveData.txt" << endl;
-	}
-	else {
-		outfile << args << endl;
-		int i = 0,j = 0;
-		if (args[0] == '-') {
-			for (i = 0; i < route1.size(); i++) {
-				outfile << route1[i] + " ";
-			}
-			outfile << "" << endl;
-		}
-		else {
-			for (i = 0; i<18; i++) {
-				if (rm.r[i].routeName == args) {
-					//cout<< name<<endl;
-					for (j = 0; j<rm.r[i].stationNumber; j++) {
-						outfile << rm.r[i].stations.at(j) +" ";
-					}
-					break;
-				}
-			}
-			outfile << "" << endl;
-		}
-		outfile.close();
-	}
-	
-}
 
 void display() {
 
 	glClearColor(1.0, 1.0, 1.0, 0.0);
 	glClear(GL_COLOR_BUFFER_BIT);
 	int i = 0,j = 0;
-
 	//画线
 	station s0, s1;
 	float x0, y0, x1, y1;
 	glColor3f(0.44f, 0.50f, 0.56f);
 	glLineWidth(2.0f);
 	for (i = 0; i < 18; i++) {
-		for (j = 0; j < rm.r[i].stationNumber - 1; j++) {
-			s0 = rm.getStation(rm.r[i].stations[j]);
-			s1 = rm.getStation(rm.r[i].stations[j + 1]);
+		for (j = 0; j < sn._map.r[i].stationNumber - 1; j++) {
+			s0 = sn._map.getStation(sn._map.r[i].stations[j]);
+			s1 = sn._map.getStation(sn._map.r[i].stations[j + 1]);
 			x0 = s0.x / 26.0f;
 			y0 = s0.y / 26.0f;
 			x1 = s1.x / 26.0f;
@@ -230,10 +122,10 @@ void display() {
 	//画点
 	float x = 0;
 	float y = 0;
-	for (i = 0; i<rm.stationQueen.size(); i++) {
-		x = rm.stationQueen[i].x / 26.0f;
-		y = rm.stationQueen[i].y / 26.0f;
-		if (rm.stationQueen[i].TraSta) {
+	for (i = 0; i<sn._map.stationQueen.size(); i++) {
+		x = sn._map.stationQueen[i].x / 26.0f;
+		y = sn._map.stationQueen[i].y / 26.0f;
+		if (sn._map.stationQueen[i].TraSta) {
 			glColor3f(0.11f, 0.56f, 1.0f);
 			glBegin(GL_POLYGON);
 			for (j = 0; j<n; j++)
@@ -251,7 +143,7 @@ void display() {
 		selectFont(10, DEFAULT_CHARSET, "华文仿宋");
 		glColor3f(0.0f, 0.0f, 0.0f);
 		glRasterPos2f(x, y);
-		const char *name = rm.stationQueen[i].name.c_str();
+		const char *name = sn._map.stationQueen[i].name.c_str();
 		drawCNString(name);
 	}
 	
@@ -310,10 +202,10 @@ void animation() {
 	glColor3f(1.0f, 0.0f, 0.0f);
 	glLineWidth(3.0f);
 	for (i = 0; i < step; i++) {
-		x0 = rm.getStation(route1[i]).x / 26.0f;
-		y0 = rm.getStation(route1[i]).y / 26.0f;
-		x1 = rm.getStation(route1[i+1]).x / 26.0f;
-		y1 = rm.getStation(route1[i+1]).y / 26.0f;
+		x0 = sn._map.getStation(sn.route1[i]).x / 26.0f;
+		y0 = sn._map.getStation(sn.route1[i]).y / 26.0f;
+		x1 = sn._map.getStation(sn.route1[i+1]).x / 26.0f;
+		y1 = sn._map.getStation(sn.route1[i+1]).y / 26.0f;
 		glBegin(GL_LINES);
 		glVertex2f(x0, y0);
 		glVertex2f(x1, y1);
@@ -322,8 +214,8 @@ void animation() {
 
 	//走过的站点 黄色
 	for (i = 0; i < step; i++) {
-		x = rm.getStation(route1[i]).x / 26.0f;
-		y = rm.getStation(route1[i]).y / 26.0f;
+		x = sn._map.getStation(sn.route1[i]).x / 26.0f;
+		y = sn._map.getStation(sn.route1[i]).y / 26.0f;
 		glColor3f(1.0f, 1.0f, 0.0f);
 		glBegin(GL_POLYGON);
 		for (j = 0; j<n; j++)
@@ -332,8 +224,8 @@ void animation() {
 	}
 
 	//当前站点 浅绿色
-	x = rm.getStation(route1[step]).x / 26.0f;
-	y = rm.getStation(route1[step]).y / 26.0f;
+	x = sn._map.getStation(sn.route1[step]).x / 26.0f;
+	y = sn._map.getStation(sn.route1[step]).y / 26.0f;
 	glColor3f(0.0f, 1.0f, 0.0f);
 	glBegin(GL_POLYGON);
 	for (j = 0; j<n; j++)
@@ -347,7 +239,7 @@ void animation() {
 //设置刷新频率频率
 void OnTimer(int value)
 {
-	if (step < route1.size() - 1) {
+	if (step < sn.route1.size() - 1) {
 		step++;
 		glutPostRedisplay();
 		glutTimerFunc(1000, OnTimer, 1);
--- SubwayRoute/SubwayRoute/subwayNet.h
+++ SubwayRoute/SubwayRoute/subwayNet.h
@@ -15,17 +15,29 @@
 
 class subwayNet {
 private:
-	ReadMap _map;
 	calculateRoute _calRoute;
+	vector<vector<string>> route;
+	int bt = 0; //-b的次数
+	int ct = 0; //-c的次数
+	
 public:
-	subwayNet();
-	~subwayNet();
-	//输出线路上的站点
-	string getStations(string name);
-	//最短距离寻路
-	string getRouteB(string begin, string end);
-	//最小换乘寻路
-	string getRouteC(string begin, string end);
+	ReadMap _map;
+	vector<string> route1;     //通过计算得到的路线,不带换乘信息
+	vector<string> route2;     //通过计算得到的路线,带换乘信息
+	subwayNet() {
+		_map.init();
+		_calRoute.init(_map);
+	}
 	//存储信息
-	void saveData(string data);
+	void saveData(string &args);
+	//输出线路上的站点
+	void getStations(string &name);
+	//最短距离寻路
+	bool getRouteB(string &begin, string &end);
+	//最小换乘寻路
+	bool getRouteC(string &begin, string &end);
+	//画图
+	bool getRouteG(string &begin, string &end);
+	
+	
 };
