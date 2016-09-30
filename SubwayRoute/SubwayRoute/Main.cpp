ffd4414ec84a274b243b20767b5e71614b9e1135
ChengFR <459763742@qq.com>
2016-09-30 16:46:16 +0800

    异常的定义

--- SubwayRoute/SubwayRoute/Main.cpp
+++ SubwayRoute/SubwayRoute/Main.cpp
@@ -11,6 +11,7 @@
 #include <string>
 #include <iostream>
 #include <time.h>
+#include "MyException.h"
 using namespace std;
 
 
---
+++ SubwayRoute/SubwayRoute/MyException.h
@@ -0,0 +1,47 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+class FileNotExistException {
+public:
+	FileNotExistException() {
+		std::cerr << "读取文件不存在" << std::endl;
+	}
+	FileNotExistException(std::string filename) {
+		std::cerr << "读取文件:"+filename+" 不存在" << std::endl;
+	}
+};
+
+class MapPatterException {
+public:
+	MapPatterException() {
+		std::cerr << "地图格式错误" << std::endl;
+	}
+};
+
+class LineNotExistException {
+public:
+	LineNotExistException() {
+		std::cerr << "线路不存在" << std::endl;
+	}
+	LineNotExistException(std::string line) {
+		std::cerr << line + "不存在" << std::endl;
+	}
+};
+
+class StationNotExistException {
+public:
+	StationNotExistException() {
+		std::cerr << "站点不存在" << std::endl;
+	}
+	StationNotExistException(std::string station) {
+		std::cerr << station + "不存在" << std::endl;
+	}
+};
+
+class InputPatternException {
+public:
+	InputPatternException() {
+		std::cerr << "输入格式错误" << std::endl;
+	}
+};
